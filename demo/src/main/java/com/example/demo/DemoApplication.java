3cf6af6dfb16077f7a21d00267829a06e7f74f61
JunpingChen <42672739+JunpingChen@users.noreply.github.com>
2018-10-06 16:53:10 +0800

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		// 模拟冲突
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
