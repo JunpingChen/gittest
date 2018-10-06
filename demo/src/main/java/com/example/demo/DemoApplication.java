940ac449ee898c1442daf4e804a9d4b44fc4c173
chjp <1398704556@qq.com>
2018-10-06 16:55:51 +0800

    修改Application 类

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		// 模拟冲突
+        System.out.println( "hello world");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
